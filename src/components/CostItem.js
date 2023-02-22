ca22a634514af51bee36dbc88a7b46be57c507b1
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-02-23 00:38:43 +0300

    date edit

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -2,9 +2,17 @@ import './CostItem.css'
 
 function CostItem(props){
 
+    const month= props.date.toLocaleString("en-US",{month:"long"});
+    const year= props.date.getFullYear();
+    const day= props.date.toLocaleString("en-US",{day:"2-digit"});
+
     return(
         <div className='cost-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div> {year}</div>
+                <div> {day}</div>
+            </div>
             <div className='cost-item__description'>
             <h2>{props.description}</h2>
             <div className='cost-item__description'>${props.amount}</div>
