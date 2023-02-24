8059438609867f8eaa36390d6461076ad5d9f03a
Dima Cheshko <dima4ka.cheshko@gmail.com>
2023-02-24 17:53:53 +0300

    refact component and css edit

---
+++ src/components/CostDate.js
@@ -0,0 +1,18 @@
+import "./CostDate.css"
+
+function CostDate(props){
+
+    const month= props.date.toLocaleString("en-US",{month:"long"});
+    const year= props.date.getFullYear();
+    const day= props.date.toLocaleString("en-US",{day:"2-digit"});
+
+    return (
+        <div className='cost-date'>
+            <div className='cost-date__month'>{month}</div>
+            <div className='cost-date__year'> {year}</div>
+            <div className='cost-date__day'> {day}</div>
+        </div>
+    );
+}
+
+export default CostDate;
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,18 +1,13 @@
 import './CostItem.css'
+import CostDate from "./CostDate.js"
 
 function CostItem(props){
 
-    const month= props.date.toLocaleString("en-US",{month:"long"});
-    const year= props.date.getFullYear();
-    const day= props.date.toLocaleString("en-US",{day:"2-digit"});
+
 
     return(
         <div className='cost-item'>
-            <div>
-                <div>{month}</div>
-                <div> {year}</div>
-                <div> {day}</div>
-            </div>
+            <CostDate date={props.date}></CostDate>
             <div className='cost-item__description'>
             <h2>{props.description}</h2>
             <div className='cost-item__description'>${props.amount}</div>
